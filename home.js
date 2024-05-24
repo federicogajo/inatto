75ee7248320dfdb70f4c810246f11b1d776b7228
Martina Esposito <martina.esposito@mail.polimi.it>
2024-05-24 08:41:13 +0200

    update

--- home.js
+++ home.js
@@ -35,27 +35,27 @@ let colors = [
 
 let home;
 
-let hero;
-let heroPCTn;
-let heroP = "Per un nuovo ordine creativo";
-heroP = "INATTO</br>PER UN NUOVO ORDINAMENTO CREATIVO </br></br>";
-let heroTxtCtn;
-let heroTxt = [
-  // I diritti non sono solo quelli riconosciuti dalla legge. </br>
-  "Spesso ci comportiamo come se avessimo il diritto di agire in un certo modo, anche se formalmente quel diritto non esiste.",
-  "La collaborazione tra più individui, anche quando involontaria, conferisce a quei comportamenti la legittimità che le autorità non gli riconoscono. Un diritto, per essere definito tale, deve essere condiviso. Di conseguenza, dal momento in cui vengono messi in atto da molti, questi comportamenti apparentemente arbitrari e istintivi diventano, di fatto, dei veri e propri diritti. </br></br>",
-  "INATTO è scritta da creativi per creativi, in uno scambio aperto e orizzontale, che dà voce a diversi attori e alle loro esigenze.",
-  //La coralità di punti di vista ne preserva l’integrità.
-  "Ciascuno è presentato in modo autentico e trasparente. I contenuti di INATTO vengono prodotti combinando parole e opere di diverse persone, in un dialogo reso possibile attraverso la rivista stessa.",
-  "La testata del magazine è la nostra Legislazione mentre ogni volume presenta uno dei Diritti da cui è composta.",
-];
-//   "I diritti non sono solo quelli riconosciuti dalla legge. Spesso ci comportiamo come se avessimo il permesso di agire in un certo modo, anche se formalmente quel diritto non esiste. La collaborazione tra più individui, anche quando involontaria, conferisce a quei comportamenti la legittimità che le autorità non gli riconoscono. Un diritto, per essere definito tale, deve essere condiviso. Di conseguenza, questi comportamenti, anche se in modo apparente arbitrari e istintivi, dal momento in cui vengono messi in atto da molti, diventano, di fatto, veri e propri diritti. Questo causa ripercussioni tangibili sul sistema che li attua e sulle leggi che li regolamentano. Le leggi non si adattano ai tempi, nello stesso modo con cui lo fanno i comportamenti. L’avvento di Internet, dei social media e dell’intelligenza artificiale hanno rivoluzionato il settore creativo, influenzando aspetti tecnici, metodologici, produttivi e identitari. Affinché la legge si evolva è necessaria una presa di posizione, ma soprattutto la consapevolezza di coloro che fanno diretta esperienza delle implicazioni che regolamentano il sistema. l nostro obiettivo non è sovvertire il sistema, bensì rivelare questi “diritti non-diritti” e le relative controversie. Ridefinire il significato di certi comportamenti considerati negativi, esaltandone gli aspetti positivi. Le persone che li attuano passano dall’essere attori colpevoli di pratiche mal viste, a paladini di una nuova costituzione. INATTO è scritta da creativi per creativi, in uno scambio aperto e orizzontale, riuscendo a dare voce a diversi attori e alle loro esigenze. La coralità di voci preserva l’integrità di ciascun punto di vista, che viene presentato in modo autentico e trasparente. I protagonisti sono persone autorevoli, con un punto di vista dato dal loro coinvolgimento nel settore. La collettività dà origine ai contenuti di INATTO, i quali vengono prodotti combinando voci e opere di diverse persone, creando un dialogo attraverso la rivista stessa. La testata del magazine è la nostra Legislazione, dove ogni volume presenta uno dei Diritti da cui è composta. Ogni numero è suddiviso in Titoli, corrispondenti alle sezioni principali, a loro volta suddivise in Capi, contenenti i diversi Articoli. Le tematiche trattate sono a sostegno del diritto in questione. Se ad uno stesso argomento sono associati più Articoli, vengono indicati come Articolo N. e Articolo N. Bis. Il Volume del 27.05.24 discute del Diritto di copiare.";
+// let hero;
+// let heroPCTn;
+// let heroP = "Per un nuovo ordine creativo";
+// heroP = "INATTO</br>PER UN NUOVO ORDINAMENTO CREATIVO </br></br>";
+// let heroTxtCtn;
+// let heroTxt = [
+//   // I diritti non sono solo quelli riconosciuti dalla legge. </br>
+//   "Spesso ci comportiamo come se avessimo il diritto di agire in un certo modo, anche se formalmente quel diritto non esiste.</br></br>",
+//   //   "La collaborazione tra più individui, anche quando involontaria, conferisce a quei comportamenti la legittimità che le autorità non gli riconoscono. Un diritto, per essere definito tale, deve essere condiviso. Di conseguenza, dal momento in cui vengono messi in atto da molti, questi comportamenti apparentemente arbitrari e istintivi diventano, di fatto, dei veri e propri diritti. </br></br>",
+//   "INATTO è una rivista indipendente scritta da creativi per creativi, in uno scambio aperto e orizzontale, che dà voce a diversi attori e alle loro esigenze</br></br>",
+//   //La coralità di punti di vista ne preserva l’integrità.
+//   //   "Ciascuno è presentato in modo autentico e trasparente. I contenuti di INATTO vengono prodotti combinando parole e opere di diverse persone, in un dialogo reso possibile attraverso la rivista stessa.",
+//   "La testata del magazine è la nostra Legislazione mentre ogni volume presenta uno dei Diritti da cui è composta.",
+// ];
+// //   "I diritti non sono solo quelli riconosciuti dalla legge. Spesso ci comportiamo come se avessimo il permesso di agire in un certo modo, anche se formalmente quel diritto non esiste. La collaborazione tra più individui, anche quando involontaria, conferisce a quei comportamenti la legittimità che le autorità non gli riconoscono. Un diritto, per essere definito tale, deve essere condiviso. Di conseguenza, questi comportamenti, anche se in modo apparente arbitrari e istintivi, dal momento in cui vengono messi in atto da molti, diventano, di fatto, veri e propri diritti. Questo causa ripercussioni tangibili sul sistema che li attua e sulle leggi che li regolamentano. Le leggi non si adattano ai tempi, nello stesso modo con cui lo fanno i comportamenti. L’avvento di Internet, dei social media e dell’intelligenza artificiale hanno rivoluzionato il settore creativo, influenzando aspetti tecnici, metodologici, produttivi e identitari. Affinché la legge si evolva è necessaria una presa di posizione, ma soprattutto la consapevolezza di coloro che fanno diretta esperienza delle implicazioni che regolamentano il sistema. l nostro obiettivo non è sovvertire il sistema, bensì rivelare questi “diritti non-diritti” e le relative controversie. Ridefinire il significato di certi comportamenti considerati negativi, esaltandone gli aspetti positivi. Le persone che li attuano passano dall’essere attori colpevoli di pratiche mal viste, a paladini di una nuova costituzione. INATTO è scritta da creativi per creativi, in uno scambio aperto e orizzontale, riuscendo a dare voce a diversi attori e alle loro esigenze. La coralità di voci preserva l’integrità di ciascun punto di vista, che viene presentato in modo autentico e trasparente. I protagonisti sono persone autorevoli, con un punto di vista dato dal loro coinvolgimento nel settore. La collettività dà origine ai contenuti di INATTO, i quali vengono prodotti combinando voci e opere di diverse persone, creando un dialogo attraverso la rivista stessa. La testata del magazine è la nostra Legislazione, dove ogni volume presenta uno dei Diritti da cui è composta. Ogni numero è suddiviso in Titoli, corrispondenti alle sezioni principali, a loro volta suddivise in Capi, contenenti i diversi Articoli. Le tematiche trattate sono a sostegno del diritto in questione. Se ad uno stesso argomento sono associati più Articoli, vengono indicati come Articolo N. e Articolo N. Bis. Il Volume del 27.05.24 discute del Diritto di copiare.";
 
-let header;
-let headerE;
-let headerIssue;
-let headerTime;
-let headerPlace;
+// let header;
+// let headerE;
+// let headerIssue;
+// let headerTime;
+// let headerPlace;
 
 //HOME BTN
 home = document.createElement("a");
@@ -64,30 +64,30 @@ home.classList.add("home");
 home.href = "index.html";
 document.body.appendChild(home);
 
-//HERO
-hero = document.createElement("div");
-hero.classList.add("hero");
+// //HERO
+// hero = document.createElement("div");
+// hero.classList.add("hero");
 
-heroPCTn = document.createElement("div");
-heroPCTn.innerHTML = heroP;
-heroPCTn.classList.add("heroTimes");
-hero.appendChild(heroPCTn);
+// heroPCTn = document.createElement("div");
+// heroPCTn.innerHTML = heroP;
+// heroPCTn.classList.add("heroTimes");
+// hero.appendChild(heroPCTn);
 
-for (let i = 0; i <= heroTxt.length - 1; i++) {
-  let e = heroTxtCtn + i;
-  e = document.createElement("div");
-  e.classList.add("heroTimes");
-  e.classList.add("heroTxtCtn-" + i);
+// for (let i = 0; i <= heroTxt.length - 1; i++) {
+//   let e = heroTxtCtn + i;
+//   e = document.createElement("div");
+//   e.classList.add("heroTimes");
+//   e.classList.add("heroTxtCtn-" + i);
 
-  let eTxt = heroTxt[i];
-  //   console.log(eTxt);
-  e.innerHTML = eTxt;
+//   let eTxt = heroTxt[i];
+//   //   console.log(eTxt);
+//   e.innerHTML = eTxt;
 
-  hero.appendChild(e);
-}
+//   hero.appendChild(e);
+// }
 
-// heroTxtCtn2.style = "font-size:2rem";
-document.body.appendChild(hero);
+// // heroTxtCtn2.style = "font-size:2rem";
+// document.body.appendChild(hero);
 
 //ISSUES
 for (let i = 0; i < issuesG.length; i++) {
@@ -131,7 +131,7 @@ let footerE;
 let footerEctn;
 let footerDesCtn;
 let footerDes =
-  "INATTO è un progetto universitario sviluppato nel corso di Laboratorio di Progettazione di sistemi e artefatti complessi C1 della Laurea Magistrale in Design della Comunicazione del Politecnico di Milano. </br>Il macrotema del corso è la collaborazione, mentre il sottotema del progetto sono i diritti. </br>Il progetto nasce con l'intento di riflettere sul valore sociale dell'editoria indipendente. ";
+  "INATTO è un progetto universitario sviluppato nel corso di Laboratorio di Progettazione di sistemi e artefatti complessi C1 della Laurea Magistrale in Design della Comunicazione del Politecnico di Milano. Il corso punta a riflettere sul valore sociale dell'editoria indipendente.</br>Il macrotema del progetto è la collaborazione, mentre il sottotema sono i diritti. </br></br>La rivista è scritta da creativi per creativi, con l'obiettivo di aprire una discussione su quelle che sono le esigenze di chi appartiene a questo settore. </br> La testata è la legislazione mentre ogni numero è nuovo diritto che viene legittimato attraverso le parole delle persone che hanno scritto gli articoli. </br> La rivista rende possibile un dialogo tra punti di vista diversi ma comunque importanti, nello scopo di discutere quei tabù che impediscono al sistema di evolversi.";
 let footerTxt = [
   {
     sezione: "CCO–Redazione:</br>",
